243f433f710a7a38fc904453b5438d251b1fde8d
whikih34 <gitacc34@gmail.com>
2024-08-31 17:52:52 +0900

    Solution

--- src/Number1000.java
+++ src/Number1000.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Number1000 {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + b);
   }
 }
---
+++ src/Number2557.java
@@ -0,0 +1,5 @@
+public class Number2557 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
